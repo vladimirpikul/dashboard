7ef57d5c85dc1954e019e37259e4b0b3c84805ad
vladimirpikul <vladimir.pikul.1985@gmail.com>
2017-10-26 23:53:43 +0300

    добавили календарь на главной; начали страницу Links...

---
+++ src/js/plugin.js
@@ -0,0 +1,13 @@
+;(function ($) {
+
+    // dropdown
+    var dropdownWrap = $('.dropdown-wrap');
+
+    dropdownWrap.on('click', function (e){
+        e.preventDefault();
+        var target = $(this).find('.dropdown-menu');
+        target.slideToggle();
+        $(this).toggleClass('open');
+    });
+
+})(jQuery);
