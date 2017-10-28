8323fb824c3158cdca54ff8e01ca7eec7402e42f
vladimirpikul <vladimir.pikul.1985@gmail.com>
2017-10-28 16:03:06 +0300

    начали модальное окно, поля ввода...

--- src/js/plugin.js
+++ src/js/plugin.js
@@ -10,4 +10,35 @@
         $(this).toggleClass('open');
     });
 
+    // toggle sidebar menu
+    var menuBtn = $('.menu-btn');
+    var body = $('body');
+    var mobileClass = '_mobileSidebar';
+    var openInMobile = '_openInMobile';
+    var mobileBreakPoint = 1280;
+
+    menuBtn.on('click', function (e){
+        var width = $(window).width();
+        body.toggleClass(mobileClass);
+        if(width < mobileBreakPoint){
+            body.toggleClass(openInMobile)
+        }
+    });
+
+    $(window).on('resize load', function (e) {
+        var width = $(this).width();
+        if(width < mobileBreakPoint && !body.hasClass(openInMobile)){
+            body.addClass(mobileClass);
+        } else if (width >= mobileBreakPoint) {
+            body.removeClass(mobileClass);
+            body.removeClass(openInMobile);
+        }
+    });
+
+    // loader hide
+    var loader = $('#loading');
+    $(window).on('load', function (e) {
+        loader.addClass('hide');
+    })
+
 })(jQuery);
