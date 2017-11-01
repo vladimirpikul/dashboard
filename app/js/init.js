71bab162fc4f36bf507066e17d9ffd5c4547e760
vladimirpikul <vladimir.pikul.1985@gmail.com>
2017-11-01 21:34:50 +0200

    сделали чекбоксы в модальном окне Links, подключили скролл; тень dropdown menu

--- app/js/init.js
+++ app/js/init.js
@@ -36,9 +36,53 @@ $( function() {
     modalBtn.on('click', function (e) {
         e.preventDefault();
         var target = $(this).attr('data-target');
-        $(target).bPopup();
-    })
+        $(target).bPopup({
+            closeClass: 'close'
+        });
+    });
 
     // form element styler init
     $('select').styler();
+
+    // scrollbar init
+    $('.scrollbar-inner').scrollbar();
+
+    // chart init
+    var chartBlock = document.getElementById('myChart');
+    if(chartBlock){
+        var myChart = new Chart(chartBlock, {
+            type: 'bar',
+            data: {
+                labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09'],
+                datasets: [
+                    {
+                        label: 'Signups',
+                        data: [100, 200, 220, 170, 130, 90, 157, 180, 155],
+                        backgroundColor: 'rgba(128, 191, 202, 1)'
+                    },
+                    {
+                        label: 'FTD',
+                        data: [200, 120, 180, 142, 121, 98, 140, 136, 100],
+                        backgroundColor: 'rgba(255, 154, 56, 1)'
+                    },
+                    {
+                        label: 'Earned',
+                        data: [190, 150, 200, 130, 160, 77, 150, 167, 90],
+                        backgroundColor: '#59b66d'
+                    }
+                ]
+            },
+            options: {
+                responsive: true,
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        })
+    }
+
 } );
